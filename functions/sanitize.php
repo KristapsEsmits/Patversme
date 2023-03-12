b389e96aca607a96a85efeae2bd8b21157df2549
Kristaps <esmitskristaps@inbox.lv>
2023-03-12 18:05:21 +0200

    config get password not working so i lef it empty (Access denied for user 'root'@'localhost' (using password: YES))

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,4 +1,5 @@
 <?php
+#function to prevent XSS (cross-site scripting) attacks by encoding potentially malicious characters (<, >, &, ", ')
 function escape($string)
 {
     return htmlentities($string, ENT_QUOTES, 'UTF-8');
