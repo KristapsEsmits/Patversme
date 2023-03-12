bac44b5a19e467cde456a3a81d57cbe4770015da
Kristaps <esmitskristaps@inbox.lv>
2023-03-12 16:51:50 +0200

    init.php done

---
+++ core/init.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+    #Mysql settings
+    'mysql' => array(
+        #127.0.0.1 - so you dont need to do dns lookup
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'pasword' => '',
+        'db' => 'patversme'
+    ),
+    #How long users will be remembered (cookie expiry)
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ),
+    #Session name and token
+    'session' => array(
+        'session_name' => 'user'
+    )
+);
+
+#Includes all classes files (Allows to pass function so require_once is used only once)
+spl_autoload_register(function ($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+#Includes functions
+require_once 'fuctions/sanitize.php';
