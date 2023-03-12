c194fb3af9eac3c41e2f8a2429da06b44ed7e3d1
Kristaps <esmitskristaps@inbox.lv>
2023-03-12 17:19:53 +0200

    global array config get

--- classes/Config.php
+++ classes/Config.php
@@ -3,11 +3,16 @@ class Config
 {
     public static function get($path = null) #Null so can check if exists
     {
-        if ($path) { #Where is cofing comming from
-            $config = $GLOBALS['config'];
-            $path = explode('/', $path);
+        if ($path) { #Checks if $path is not null
+            $config = $GLOBALS['config']; #Gets the global config array
+            $path = explode('/', $path); #Splits the $path string into an array
 
-            print_r($path);
+            foreach ($path as $bit) {
+                if (isset($config[$bit])) { #Checks if the current $bit element exists in the $config
+                    $config = $config[$bit];
+                }
+            }
+            return $config;
         }
     }
 }
