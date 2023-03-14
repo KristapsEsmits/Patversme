c0623629ee94589eb66633b1bc961c47a225e6d5
Kristaps <esmitskristaps@inbox.lv>
2023-03-14 19:42:49 +0200

    jaraksta komentari uzreiz savadak aizmirst kur kaas ka stradaja

--- classes/Input.php
+++ classes/Input.php
@@ -3,11 +3,15 @@ class Input
 {
     public static function exists($type = 'post')
     {
+        #A switch statement to check the value of $type
         switch ($type) {
+            #If $type is 'post', return true if $_POST is not empty, and false otherwise
             case 'post':
                 return (!empty($_POST)) ? true : false;
+            #If $type is 'get', return true if $_GET is not empty, and false otherwise
             case 'get':
                 return (!empty($_POST)) ? true : false;
+            #For any other value of $type, return false
             default:
                 return false;
         }
