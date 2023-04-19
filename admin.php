ec00864bdde07f3e935a87b4d7dd72a8b5c6f6ad
Kristaps <esmitskristaps@inbox.lv>
2023-04-19 23:32:40 +0300

    all visit panel

--- admin.php
+++ admin.php
@@ -29,6 +29,7 @@ if (!$user->isLoggedIn() || !$user->hasPermission('admin')) {
         <button onclick="window.location.href='addanimals.php'">Pievienot dzīvnieku</button>
         <button onclick="window.location.href='animalslist.php'">Apskatīt dzīvniekums</button>
         <button onclick="window.location.href='users.php'">Apskatīt lietotājus</button>
+        <button onclick="window.location.href='allVisits.php'">Apskatīt vizītes</button>
     </section>
     <?php include 'includes/footer.php' ?>
     <script src="resources/js/bootstrap.bundle.min.js"></script>
---
+++ allVisits.php
@@ -0,0 +1,86 @@
+<?php
+require_once 'core/init.php';
+
+$user = new User();
+
+if (!$user->isLoggedIn() || !$user->hasPermission('admin')) {
+    Redirect::to('index.php');
+}
+
+include 'includes/databaseCon.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jaunās Mājas: Kontakti</title>
+    <link rel="icon" href="resources/img/fav.png" />
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="resources/css/table.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <?php include 'includes/nav.php'; ?>
+    <div class="content">
+        <div class="container">
+            <h2>Plānotās vizītes:</h2>
+            <table class="table custom-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Datums</th>
+                        <th scope="col">Vārds</th>
+                        <th scope="col">Uzvārds</th>
+                        <th scope="col">Tel nr</th>
+                        <th scope="col">epasts</th>
+                        <th scope="col">Dzīvnieks</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT visit.*, users.* 
+                              FROM visit 
+                              JOIN users ON visit.id = users.id 
+                              WHERE visit.date >= CURDATE()";
+                    $result = mysqli_query($con, $query);
+                    while ($row = mysqli_fetch_array($result)) {
+                        ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['date']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['name']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['surname']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['phone']; ?>
+                            </td>
+                            <td>
+                                <?php echo $row['email']; ?>
+                            </td>
+                            <td>
+                                <a href='animalprofile.php?animalID=<?php echo $row['animalID']; ?>'>Apskatīt</a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="resources/js/jquery-3.3.1.min.js"></script>
+    <script src="resources/js/main.js"></script>
+    <script src="resources/js/bootstrap.bundle.min.js"></script>
+    <?php include 'includes/footer.php' ?>
+</body>
+
+</html>
